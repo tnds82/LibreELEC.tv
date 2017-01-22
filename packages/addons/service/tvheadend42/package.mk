c02945c762d8377bde93da05f73ab211733b6e43
tnds82 <tiagondasilva@gmail.com>
2017-01-22 14:18:21 +0000

    Tvheadend42: update to 4.1.2415

--- packages/addons/service/tvheadend42/package.mk
+++ packages/addons/service/tvheadend42/package.mk
@@ -17,9 +17,9 @@
 ################################################################################
 
 PKG_NAME="tvheadend42"
-PKG_VERSION="041b949"
-PKG_VERSION_NUMBER="4.1.2414"
-PKG_REV="210"
+PKG_VERSION="	e5f5a42"
+PKG_VERSION_NUMBER="4.1.2415"
+PKG_REV="211"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
