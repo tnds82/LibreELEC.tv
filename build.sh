66d53e969c83c7aec8b3cca320a75e3b75477867
Ntemis <ierokipides.dem@gmail.com>
2017-03-06 15:11:47 +0200

    Switch builds from DEVICE to SYSTEM

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 rm -rf target/
 
 >&2 echo "S802.arm"
-PROJECT=S802 DEVICE=S82 ARCH=arm make amlpkg -j8
-PROJECT=S802 DEVICE=M8 ARCH=arm make amlpkg -j8
-PROJECT=S802 DEVICE=T8 ARCH=arm make amlpkg -j8
-PROJECT=S802 DEVICE=MXIII-1G ARCH=arm make amlpkg -j8
+PROJECT=S802 SYSTEM=S82 ARCH=arm make image -j8
+PROJECT=S802 SYSTEM=M8 ARCH=arm make image -j8
+PROJECT=S802 SYSTEM=T8 ARCH=arm make image -j8
+PROJECT=S802 SYSTEM=MXIII-1G ARCH=arm make image -j8
 >&2 echo "S805.arm"
-PROJECT=S805 DEVICE=HD18Q ARCH=arm make amlpkg -j8
+PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
 
 rm target/*.kernel
 rm target/*.system
