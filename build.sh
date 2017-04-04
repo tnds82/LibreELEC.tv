899dd54acf5e4f6c8a55dc713ba7e67e68825c7a
Ntemis <ierokipides.dem@gmail.com>
2017-04-04 14:29:58 +0300

    AML8726:Upgrade kernel to latest LTS & fix kernel config

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make image -j8
 PROJECT=S8X2 SYSTEM=M8S-PLUS ARCH=arm make image -j8
 >&2 echo "S805.arm"
 PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
+>&2 echo "AML8726.arm"
+PROJECT=AML8726 ARCH=arm make image -j8
 
 rm target/*.kernel
 rm target/*.system
