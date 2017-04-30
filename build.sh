34dd044b65676a48743092441747cf43bb72282c
Ntemis <ierokipides.dem@gmail.com>
2017-04-30 21:09:53 +0300

    Revert "Add k200C 1GB box support(S802)"
    This reverts commit 7743bda91a633cdfe5b3c889165798847a0a3012.

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ PROJECT=S8X2 SYSTEM=M8 ARCH=arm make amlpkg -j8
 PROJECT=S8X2 SYSTEM=T8 ARCH=arm make amlpkg -j8
 PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make amlpkg -j8
 PROJECT=S8X2 SYSTEM=M8S-PLUS ARCH=arm make amlpkg -j8
-PROJECT=S8X2 SYSTEM=MXIII-PLUS ARCH=arm make amlpkg -j8
 >&2 echo "S805.arm"
 PROJECT=S805 SYSTEM=HD18Q ARCH=arm make amlpkg -j8
 >&2 echo "AML8726.arm"
