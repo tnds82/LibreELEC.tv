b4f57d62dd6c02fb2c82d3a668e896ff89728de7
Ntemis <ierokipides.dem@gmail.com>
2017-05-24 01:04:11 +0300

    Adjust and fix build script

--- build.sh
+++ build.sh
@@ -3,17 +3,16 @@
 rm -rf target/
 
 >&2 echo "S8X2.arm"
-PROJECT=S8X2 SYSTEM=S82 ARCH=arm make amlpkg -j8
-PROJECT=S8X2 SYSTEM=M8 ARCH=arm make amlpkg -j8
-PROJECT=S8X2 SYSTEM=T8 ARCH=arm make amlpkg -j8
-PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make amlpkg -j8
-PROJECT=S8X2 DEVICE=M8S-PLUS ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=S82 ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=M8 ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=T8 ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=MXIII-PLUS ARCH=arm make image -j8
+PROJECT=S8X2 SYSTEM=X8H-PLUS ARCH=arm make image -j8
 >&2 echo "S805.arm"
-PROJECT=S805 SYSTEM=HD18Q ARCH=arm make amlpkg -j8
->&2 echo "AML8726.arm"
-PROJECT=AML8726 ARCH=arm make amlpkg -j8
+PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
 
-PROJECT=H3 SYSTEM=opipc ARCH=arm make image -j8
+#PROJECT=H3 SYSTEM=opipc ARCH=arm make image -j8
 
 rm -rf target/*.kernel
 rm -rf target/*.system
