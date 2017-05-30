71b9ad572b1f9a3cdde92cedc49bfd931f9e7786
afl1 <afl2001@gmail.com>
2017-05-30 13:31:58 +0200

    Merge pull request #1634 from vpeter4/imx6_options_8

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="libcec"
-PKG_VERSION="2fc92b5"
-PKG_REV="1"
+PKG_VERSION="0a97062"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_URL="https://github.com/Pulse-Eight/libcec/archive/$PKG_VERSION.tar.gz"
+[ "$PROJECT" = "imx6" ] && PKG_PATCH_DIRS="${LINUX#imx6-}"
 PKG_DEPENDS_TARGET="toolchain systemd lockdev p8-platform"
 PKG_SECTION="system"
 PKG_SHORTDESC="libCEC is an open-source dual licensed library designed for communicating with the Pulse-Eight USB - CEC Adaptor"
@@ -32,10 +32,8 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
-                       -DCMAKE_INSTALL_LIBDIR=/usr/lib \
-                       -DCMAKE_INSTALL_LIBDIR_NOARCH=/usr/lib \
-                       -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
-                       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr"
+                       -DCMAKE_INSTALL_LIBDIR:STRING=lib \
+                       -DCMAKE_INSTALL_LIBDIR_NOARCH:STRING=lib"
 
 if [ "$KODIPLAYER_DRIVER" = "bcm2835-firmware" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-firmware"
