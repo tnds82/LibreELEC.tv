333c230ad489c4a81886067362f662480c8bfb52
root <root@sd-59788.dedibox.fr>
2017-12-10 00:36:45 +0100

    tnds82: initial addon

---
+++ packages/addons/service/tnds82/package.mk
@@ -0,0 +1,43 @@
+################################################################################
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tnds82"
+PKG_REV="1"
+PKG_SITE="http://tnds82.xyz"
+PKG_SECTION="service"
+PKG_AUTORECONF="no"
+PKG_ADDON_NAME="Tnds82 Add-ons"
+PKG_SHORTDESC="$PKG_ADDON_NAME: add-on repository"
+PKG_LONGDESC="$PKG_ADDON_NAME: add-on repository, provides Tvhwizard, Tvheadend42, OSCam, Plex Media Server, Lamp, 3rd Party Repositorys."
+PKG_DISCLAIMER="Keep it legal and carry on"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="tnds.service"
+PKG_MAINTAINER="Tnds82 (tnds82)"
+
+make_target() {
+  : # nothing to do here
+}
+
+makeinstall_target() {
+  : # nothing to do here
+}
+
+addon() {
+  : # nothing to do here
+}
---
+++ packages/addons/service/tnds82/source/default.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-2017 Team LibreELEC
+#      Copyright (C) 2017 Tnds82 (tndsrepo@gmail.com)
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from urlparse import urlparse
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xml.etree.ElementTree as etree
+
+REPO_LE = 'repository.libreelec.tv'
+REPO_TNDS = 'http://tnds82.xyz/tnds_addons/'
+
+try:
+  addon = xbmcaddon.Addon()
+  strings = addon.getLocalizedString
+
+  xml_le = xbmcaddon.Addon(REPO_LE).getAddonInfo('path') + '/addon.xml'
+  path = urlparse(etree.parse(xml_le).iter(tag='datadir').next().text).path.strip('/').split('/')
+  vpa = path[-3] + '/' +  path[-2] + '/' +  path[-1] + '/'
+  new = REPO_TNDS + vpa
+  
+  xml_tnds = addon.getAddonInfo('path') + '/addon.xml'
+  xml = etree.parse(xml_tnds)
+  old = xml.iter(tag='datadir').next()
+  if old.text == new:
+    addon.setSetting('vpa', vpa)
+  else:
+    addon.setSetting('vpa', strings(30020))
+    old.text = new
+    xml.iter(tag='info').next().text = new + 'addons.xml'
+    xml.iter(tag='checksum').next().text = new + 'addons.xml.md5'
+    xml.write(xml_tnds)
+    if xbmcgui.Dialog().yesno(addon.getAddonInfo('name'),
+                              strings(30010),
+                              strings(30011),
+                              nolabel=strings(30012),
+                              yeslabel=strings(30013)) == False:
+      xbmc.executebuiltin('RestartApp')
+
+except:
+  addon.setSetting('vpa', strings(30021))
