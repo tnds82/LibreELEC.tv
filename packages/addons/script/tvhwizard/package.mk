fcde314810aa7473f9ad5fff98bcb667741bc3a4
root <root@sd-59788.dedibox.fr>
2017-12-10 00:16:45 +0100

    tvhwizard: initial addon

---
+++ packages/addons/script/tvhwizard/package.mk
@@ -0,0 +1,48 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-2017 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tvhwizard"
+PKG_VERSION="2.0"
+PKG_VERSION_NUMBER="2.0.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://tnds82.xyz"
+PKG_URL="https://github.com/tnds82/script.tvhwizard/archive/$PKG_VERSION_NUMBER.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_SECTION="script"
+PKG_SHORTDESC="script.tvhwizard"
+PKG_LONGDESC="script.tvhwizard"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="dummy"
+
+make_target() {
+  $SED -e "s|@OS_VERSION@|$OS_VERSION|g" \
+       -e "s|@PKG_VERSION@|$PKG_VERSION|g" \
+       -i addon.xml
+}
+
+makeinstall_target() {
+  : # nothing to do here
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
+   cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID
+}
